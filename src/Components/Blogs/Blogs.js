e4c3f507f4d0fc4bb43eac5c35e426c0ad4dcb7e
Anirudhya <anirudhyaduyti@gmail.com>
2022-04-04 12:48:26 +0600

    page not found component

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -4,7 +4,18 @@ const Blogs = () => {
 
     return (
         <div>
-            blogs
+            <div className="p-10 m-5">
+                <h2 className="text-xl font-medium">What is ConText API</h2>
+                <p className="text-xl"></p>
+            </div>
+            <div className="p-10 m-5">
+                <h2 className="text-xl font-medium">What is semantic tagging</h2>
+                <p className="text-xl"></p>
+            </div>
+            <div className="p-10 m-5">
+                <h2 className="text-xl font-medium">Inline, Block & Inline-Block</h2>
+                <p className="text-xl"></p>
+            </div>
         </div>
     );
 };
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { EmojiSadIcon } from '@heroicons/react/outline';
+
 
 const NotFound = () => {
     return (
-        <div>
-            404
+        <div className="bg-slate-300/50 mx-10 my-5 md:my-11 rounded-lg h-96 py-10">
+            <h1 className="text-6xl md:text-[150px] font-black">404</h1>
+            <p className="text-medium text-gray-900 text-3xl pt-10">Page not found</p>
+            <EmojiSadIcon className="w-14 h-14 text-amber-600 mx-auto mt-6" />
         </div>
     );
 };
